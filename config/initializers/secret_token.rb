22c12b53e87c7b8fb8234c4bd987e68ee611d2a3
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-07-20 00:08:52 +0200

    Proyeto Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lubri::Application.config.secret_key_base = 'bd3f9bcd4e6c63844e26b7c9755b21377ad06a2c0025aa0dbfe429e9adfaf6bc651d24058eb557f18a101562d966e26c455aa303ff55ce05b2a5f19f30df5877'
